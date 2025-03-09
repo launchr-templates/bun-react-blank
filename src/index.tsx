b75c554746a263c4c6829095118d4dcbe40ad3f9
handlebauer <hi@donaldgeddes.ca>
2025-03-08 21:43:02 -0600

    Update default port to 3000

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { serve } from "bun";
 import index from "./index.html";
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 const server = serve({
   port: PORT,
